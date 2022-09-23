82c44167664f812d8f6f06eead19133550fc39ce
FamiwinHAHA <114204304+FamiwinHAHA@users.noreply.github.com>
2022-09-23 11:55:37 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,9 @@
 public class Test{
     public static void main(String[] args){
         System.out.println("args[0]");
-        System.out.println("args[1]");
+        printStr("Chris!");
     }
+     public static void printStr(String[str] args){
+        System.out.println("My Name =  " + str);
+     }
 }
